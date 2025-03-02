7baeadd596d5fba9519ff4d8947c223a81826dce
Bone-Structure <abbigreene13@gmail.com>
2025-03-02 11:43:06 -0500

    Added weather js and css code

---
+++ index.js
@@ -0,0 +1,7 @@
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
